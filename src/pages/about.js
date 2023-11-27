b83dafa8676008b2451c853394b0dd4e5e9d789b
RangCloud <mtr008@naver.com>
2023-11-27 13:40:32 +0900

    231127

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import NavBar from "../components/NavBar";
+
+const Potato = () => {
+    return (
+        <div>
+            <NavBar />
+            <h1>about us</h1>
+        </div>
+    )
+}
+
+export default Potato;
